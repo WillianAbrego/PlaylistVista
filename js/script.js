197625e71d03cf11d4a0d0abb51b32e08bb9ad1f
WillianAbrego <willianantonioabrego@gmail.com>
2021-07-08 19:15:11 -0600

    agregado nombre de usuario/id

--- js/script.js
+++ js/script.js
@@ -119,3 +119,49 @@ const MostrarCancion = (cancion) => {
     });
   };
 };
+
+//comienza usuario/administrador
+
+fetch("http://localhost:3000/api/v1/user")
+  .then((response) => response.json())
+  .then((data) => ShowUser(data));
+// .then((data) => data.map(mostrarData));
+
+const ShowUser = (user) => {
+  document.getElementById(
+    "user"
+  ).innerHTML += `${user[0].nombre} <div class="hidden">${user[0].id}</div>`;
+  //console.log(user[0].nombre);
+};
+
+// const mostrarData = (d) => {
+//   document.getElementById(
+//     "contenedor"
+//   ).innerHTML += `<article class="full-box tile artista" >
+//   <div class="full-box tile-title text-center text-titles text-uppercase">
+//       ${d.nombre}
+//   </div>
+//   <div class="hidden">${d.id}</div>
+
+// </div>
+//   <div class="full-box tile-icon text-center">
+//       <i class="zmdi zmdi-account"></i>
+//   </div>
+//   <div class="full-box tile-number text-titles">
+//       <p class="full-box">albunes</p>
+
+//   </div>
+// </article>`;
+
+//   //console.log(d.id);
+//   let arti = document.querySelectorAll(".artista");
+//   arti.forEach(function (elemento) {
+//     elemento.addEventListener("click", function () {
+//       let album = elemento.firstChild.parentNode.childNodes[3].textContent;
+//       document.getElementById("contenedor").innerHTML = "";
+//       MostrarAlbum(album);
+
+//       //console.log(album);
+//     });
+//   });
+// };
