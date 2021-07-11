2dc9f93bf7ef83c71df022783d7322e6ec17df80
WillianAbrego <willianantonioabrego@gmail.com>
2021-07-11 14:02:25 -0600

    agregado playlist sin funcionalidad solo visual

--- js/script.js
+++ js/script.js
@@ -125,43 +125,21 @@ const MostrarCancion = (cancion) => {
 fetch("http://localhost:3000/api/v1/user")
   .then((response) => response.json())
   .then((data) => ShowUser(data));
-// .then((data) => data.map(mostrarData));
 
 const ShowUser = (user) => {
   document.getElementById(
     "user"
   ).innerHTML += `${user[0].nombre} <div class="hidden">${user[0].id}</div>`;
-  //console.log(user[0].nombre);
 };
+//comienza playlist nombre y id
+fetch("http://localhost:3000/api/v1/playlist")
+  .then((response) => response.json())
+  .then((data) => data.map(ShowPlaylist));
 
-// const mostrarData = (d) => {
-//   document.getElementById(
-//     "contenedor"
-//   ).innerHTML += `<article class="full-box tile artista" >
-//   <div class="full-box tile-title text-center text-titles text-uppercase">
-//       ${d.nombre}
-//   </div>
-//   <div class="hidden">${d.id}</div>
-
-// </div>
-//   <div class="full-box tile-icon text-center">
-//       <i class="zmdi zmdi-account"></i>
-//   </div>
-//   <div class="full-box tile-number text-titles">
-//       <p class="full-box">albunes</p>
-
-//   </div>
-// </article>`;
-
-//   //console.log(d.id);
-//   let arti = document.querySelectorAll(".artista");
-//   arti.forEach(function (elemento) {
-//     elemento.addEventListener("click", function () {
-//       let album = elemento.firstChild.parentNode.childNodes[3].textContent;
-//       document.getElementById("contenedor").innerHTML = "";
-//       MostrarAlbum(album);
-
-//       //console.log(album);
-//     });
-//   });
-// };
+const ShowPlaylist = (play) => {
+  console.log(play);
+  document.getElementById("play").innerHTML += `<li>
+  <a href="#"><i class="zmdi zmdi-book zmdi-hc-fw"></i> ${play.nombre}</a>
+  <div class="hidden">${play.id}</div>
+</li>`;
+};
