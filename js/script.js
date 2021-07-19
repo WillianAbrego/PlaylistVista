e2c9728e6c095a7a083f8e9f53ef39790c8f3b45
WillianAbrego <willianantonioabrego@gmail.com>
2021-07-19 14:41:50 -0600

    agregado api youtube, falta llamarla desde funcion

--- js/script.js
+++ js/script.js
@@ -190,3 +190,17 @@ const MostrarDetalleCancion = (detalle) => {
     						</li>`;
   };
 };
+//script api Youtube
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player("ytplayer", {
+    height: "480px",
+    width: "100%",
+    videoId: "DbUCSY-Dt-0",
+  });
+}
